ebae774fab65b63645f992446d45af1c967f7870
Derek Lam <dc.lam@mail.utoronto.ca>
2019-11-22 00:04:32 -0800

    Locate bindings
    Lambda anti-cycle was failing (type I) because it was matching noSrcSpans
    against each other when FunBinds were bound via grhs_binds. Also located
    HsVars in AbsBinds because I've always been wary of the fact that they are
    noLoc'd. The latter change needs testing.

--- src/Ra/GHC/Translate.hs
+++ src/Ra/GHC/Translate.hs
@@ -19,7 +19,7 @@ bind_to_table :: LHsBind GhcTc -> [Bind]
 bind_to_table (L loc b) = case b of
   AbsBinds { abs_exports, abs_binds } ->
     let subbinds = mconcat $ bagToList $ mapBag bind_to_table abs_binds -- consider making union_sym_table just Foldable t => ...
-    in subbinds <> map (L loc . VarPat NoExt . noLoc . abe_poly &&& pure . sa_from_sym . Sym . noLoc . HsVar NoExt . noLoc . abe_mono) abs_exports
+    in subbinds <> map (L loc . VarPat NoExt . noLoc . abe_poly &&& pure . sa_from_sym . Sym . L loc . HsVar NoExt . noLoc . abe_mono) abs_exports
     
   -- AbsBindsSig { abs_sig_export, abs_sig_bind = L _ abs_sig_bind } -> 
   --   let subbinds = bind_to_table stack abs_sig_bind
@@ -30,7 +30,7 @@ bind_to_table (L loc b) = case b of
   -------------------
   -- SYM BASE CASE --
   -------------------
-  FunBind { fun_id = fun_id, fun_matches } -> [( L loc (VarPat NoExt fun_id), [sa_from_sym (Sym $ noLoc $ HsLam NoExt fun_matches) ])]
+  FunBind { fun_id = fun_id, fun_matches } -> [( L loc (VarPat NoExt fun_id), [sa_from_sym (Sym $ (HsLam NoExt fun_matches <$ fun_id)) ])]
   
   PatBind { pat_lhs = pat_lhs, pat_rhs } ->
     grhs_binds pat_rhs <> [(pat_lhs, map (sa_from_sym . Sym) (grhs_exprs pat_rhs))]
